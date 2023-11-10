fee1e1efe33248bc230f6b1ab0a84d27e8585c73
José Runín <jose.runin@sansano.usm.cl>
2023-11-09 23:57:43 -0300

    updated frontend to point to prod

--- Front/src/pages/Homes/index.jsx
+++ Front/src/pages/Homes/index.jsx
@@ -1,17 +1,16 @@
-import Layout from '@/components/Layout';
-import ItemGrid from '@/components/ItemGrid';
-import styles from './index.module.css';
-import { Col, Row, Button } from 'react-bootstrap';
-import { useEffect, useState } from 'react';
+import Layout from "@/components/Layout";
+import ItemGrid from "@/components/ItemGrid";
+import styles from "./index.module.css";
+import { Col, Row, Button } from "react-bootstrap";
+import { useEffect, useState } from "react";
 import RangeSlider from "react-range-slider-input";
 import "react-range-slider-input/dist/style.css";
 
 export default function Homes() {
+  const [ProductoGuardados, setProductoGuardados] = useState([]);
+  const [value, setValue] = useState([30, 60]);
 
-	const [ProductoGuardados, setProductoGuardados] = useState([]);
-	const [value, setValue] = useState([30, 60]);
-
-	/*
+  /*
 	useEffect(() => {
 		const prices = ProductoGuardados.map(ProductoGuardados => parseFloat(ProductoGuardados.price));
 		const minPrice = Math.min(...prices);
@@ -20,35 +19,32 @@ export default function Homes() {
 		setValue([minPrice, maxPrice]);
 	  }, [ProductoGuardados]);*/
 
-	
-	useEffect(() => {
-		fetch('http://localhost:5000/products')
-		.then((res)=> res.json())
-		.then((data)=> setProductoGuardados(data));
-	}, []);
+  useEffect(() => {
+    fetch("http://20.231.216.22/products")
+      .then((res) => res.json())
+      .then((data) => setProductoGuardados(data));
+  }, []);
 
-
-	return (
-		<Layout>
-			<Row>
-			<Col xs={12} md={2} >
-				<div className={styles["sidebar-grid"]}>
-					<h2>Filtros</h2>
-					<div>Precio</div>
-					<RangeSlider value={value} onInput={setValue} />
-					<ul className={styles["sidebar-list"]}>
-						<div class="container">
-						</div>
-					</ul>
-				</div>
-				<Button variant="primary">Filtrar</Button>
-			</Col>
-			<Col xs={12} md={10}>
-				<div className={styles["content-grid"]}>
-					<ItemGrid items ={ProductoGuardados}/>
-				</div>	
-			</Col>
-			</Row>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <Row>
+        <Col xs={12} md={2}>
+          <div className={styles["sidebar-grid"]}>
+            <h2>Filtros</h2>
+            <div>Precio</div>
+            <RangeSlider value={value} onInput={setValue} />
+            <ul className={styles["sidebar-list"]}>
+              <div class="container"></div>
+            </ul>
+          </div>
+          <Button variant="primary">Filtrar</Button>
+        </Col>
+        <Col xs={12} md={10}>
+          <div className={styles["content-grid"]}>
+            <ItemGrid items={ProductoGuardados} />
+          </div>
+        </Col>
+      </Row>
+    </Layout>
+  );
 }
--- Front/src/pages/Inventario/index.jsx
+++ Front/src/pages/Inventario/index.jsx
@@ -1,154 +1,154 @@
-import { useState, useRef, useEffect } from 'react';
-import { Row, Col, Form, Button, Modal } from 'react-bootstrap';
-import Layout from '@/components/Layout';
-import InventarioTable from './InventarioTable';
+import { useState, useRef, useEffect } from "react";
+import { Row, Col, Form, Button, Modal } from "react-bootstrap";
+import Layout from "@/components/Layout";
+import InventarioTable from "./InventarioTable";
 import RangeSlider from "react-range-slider-input";
 import "react-range-slider-input/dist/style.css";
 
 export default function Inventario() {
-	const [show, setShow] = useState(false);
-	const [selectedProduct, setSelectedProduct] = useState({});
-	const [productoGuardados, setProductoGuardados] = useState([]);
-	const [value, setValue] = useState([30, 60]);
+  const [show, setShow] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState({});
+  const [productoGuardados, setProductoGuardados] = useState([]);
+  const [value, setValue] = useState([30, 60]);
 
-	const handleClose = () => setShow(false);
-	const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-	const handleInputChange = (field, value) => {
-		setSelectedProduct((prevProduct) => ({
-		  	...prevProduct,
-		  	[field]: value,
-		}));
-	};
+  const handleInputChange = (field, value) => {
+    setSelectedProduct((prevProduct) => ({
+      ...prevProduct,
+      [field]: value,
+    }));
+  };
 
-	const handleImageChange = (e) => {
-		const file = e.target.files[0];
-		setSelectedProduct((prevProduct) => ({
-			...prevProduct,
-			image: file,
-		}));
-	};
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedProduct((prevProduct) => ({
+      ...prevProduct,
+      image: file,
+    }));
+  };
 
-	const handleSaveChanges = async () => {
-		console.log(selectedProduct);
-		const formDataToSend = new FormData();
-		formDataToSend.append('name', selectedProduct.name);
-		formDataToSend.append('price', selectedProduct.price);
-		formDataToSend.append('image', selectedProduct.image);
-		formDataToSend.append('description', selectedProduct.description);
-		formDataToSend.append('amount', selectedProduct.amount);
+  const handleSaveChanges = async () => {
+    console.log(selectedProduct);
+    const formDataToSend = new FormData();
+    formDataToSend.append("name", selectedProduct.name);
+    formDataToSend.append("price", selectedProduct.price);
+    formDataToSend.append("image", selectedProduct.image);
+    formDataToSend.append("description", selectedProduct.description);
+    formDataToSend.append("amount", selectedProduct.amount);
 
-		try {
-		const response = await fetch('http://localhost:5000/products', {
-			method: 'POST',
-			body: formDataToSend,
-		});
+    try {
+      const response = await fetch("http://20.231.216.22/products", {
+        method: "POST",
+        body: formDataToSend,
+      });
 
-		if (response.status === 200) {
-			const data = await response.json();
-			console.log('Product created:', data);
-		} else {
-			console.error('Error creating product 2:', response.statusText);
-		}
-		} catch (error) {
-		console.error('Error creating product 1:', error);
-		}
-		handleClose();
-	};
+      if (response.status === 200) {
+        const data = await response.json();
+        console.log("Product created:", data);
+      } else {
+        console.error("Error creating product 2:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error creating product 1:", error);
+    }
+    handleClose();
+  };
 
-	return (
-		<Layout>
-			<Row>
-			<Col xs={12} md={2} >
-				<div>
-					<h2>Filtros</h2>
-						<div className="title">Precio</div>
-      					<RangeSlider value={value} onInput={setValue} />
+  return (
+    <Layout>
+      <Row>
+        <Col xs={12} md={2}>
+          <div>
+            <h2>Filtros</h2>
+            <div className="title">Precio</div>
+            <RangeSlider value={value} onInput={setValue} />
 
-					<div class="container">
-					</div>
-				</div>
-				<Button variant="primary">Filtrar</Button>
-			</Col>
-			<Col xs={12} md={10}>
-			<InventarioTable/>
-			</Col>
-			</Row>
-			<Button onClick={handleShow}>Añadir Producto</Button>
-			<Modal 
-		show={show} 
-		onHide={handleClose}
-		backdrop="static"
-		keyboard={false}>
-
-			<Modal.Header closeButton>
-				<Modal.Title>Editar producto</Modal.Title>		
-			</Modal.Header>
-			<Modal.Body>
-				<form>
-					<div className="form-group">
-						<label for="name">Nombre</label>
-						<input
-							type="text"
-							className="form-control"
-							id="name"
-							placeholder="Introducir Nombre..."
-							onChange={(e) => handleInputChange('name', e.target.value)}
-						/>
-					</div>
-					<div className="form-group">
-						<label for="price">Precio</label>
-						<input
-						type="text"
-						className="form-control"
-						id="price"
-						placeholder="Introducir precio..."
-						onChange={(e) => handleInputChange('price', e.target.value)}
-						/>
-
-					</div>
-					<div className="form-group">
-						<label for="description">Descripcion</label>
-						<input
-						type="text"
-						className="form-control"
-						id="descriptiob"
-						placeholder="Introducir descripcion..."
-						onChange={(e) => handleInputChange('description', e.target.value)}
-						/>
-					</div>
-					<div className="form-group">
-						<label for="amount">Cantidad</label>
-						<input
-						type="text"
-						className="form-control"
-						id="amount"
-						placeholder="Introducir numero..."
-						onChange={(e) => handleInputChange('amount', e.target.value)}
-						/>
-					</div>
-					<div className="form-group">
-						<label htmlFor="image">Imagen</label>
-						<input
-							type="file"
-							accept="image/*"
-							className="form-control"
-							id="image"
-							onChange={handleImageChange}
-							required
-						/>
-        			</div>
-				</form>
-			</Modal.Body>
-			<Modal.Footer>
-				<Button variant="secondary" onClick={handleClose}>
-					Cerrar
-				</Button>
-				<Button variant="primary" onClick={handleSaveChanges}>
-					Guardar cambios
-				</Button>
-			</Modal.Footer>
-		</Modal>
-		</Layout>
-	);
+            <div class="container"></div>
+          </div>
+          <Button variant="primary">Filtrar</Button>
+        </Col>
+        <Col xs={12} md={10}>
+          <InventarioTable />
+        </Col>
+      </Row>
+      <Button onClick={handleShow}>Añadir Producto</Button>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Editar producto</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form>
+            <div className="form-group">
+              <label for="name">Nombre</label>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                placeholder="Introducir Nombre..."
+                onChange={(e) => handleInputChange("name", e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label for="price">Precio</label>
+              <input
+                type="text"
+                className="form-control"
+                id="price"
+                placeholder="Introducir precio..."
+                onChange={(e) => handleInputChange("price", e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label for="description">Descripcion</label>
+              <input
+                type="text"
+                className="form-control"
+                id="descriptiob"
+                placeholder="Introducir descripcion..."
+                onChange={(e) =>
+                  handleInputChange("description", e.target.value)
+                }
+              />
+            </div>
+            <div className="form-group">
+              <label for="amount">Cantidad</label>
+              <input
+                type="text"
+                className="form-control"
+                id="amount"
+                placeholder="Introducir numero..."
+                onChange={(e) => handleInputChange("amount", e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="image">Imagen</label>
+              <input
+                type="file"
+                accept="image/*"
+                className="form-control"
+                id="image"
+                onChange={handleImageChange}
+                required
+              />
+            </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cerrar
+          </Button>
+          <Button variant="primary" onClick={handleSaveChanges}>
+            Guardar cambios
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </Layout>
+  );
 }
