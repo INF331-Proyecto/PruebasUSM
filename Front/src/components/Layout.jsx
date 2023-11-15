8e68b41579c97b4cdbf090ce2dc3d0b4a1db2e32
guisos <guisos@guisos.myguest.virtualbox.org>
2023-11-15 12:57:10 -0300

    Sistema de Cuentas Y usuarios

--- Front/src/components/Layout.jsx
+++ Front/src/components/Layout.jsx
@@ -1,11 +1,12 @@
 import { Container } from 'react-bootstrap';
 import Navbar from './Navbar';
+import { CartProvider } from '@/context/context';
 
 export default function Layout({ children }) {
 	return (
-		<>
+		<CartProvider>
 			<Navbar />
 			<Container fluid>{children}</Container>
-		</>
+		</CartProvider>
 	);
 }
