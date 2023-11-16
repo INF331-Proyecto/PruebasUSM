b1a64a13176872ef0b64ba3c8ba897b28bd66b45
C0t300 <jose@runin.cl>
2023-11-15 22:40:45 -0300

    updated Filtros to Filtro

--- Front/src/pages/Homes/index.jsx
+++ Front/src/pages/Homes/index.jsx
@@ -7,10 +7,10 @@ import RangeSlider from "react-range-slider-input";
 import "react-range-slider-input/dist/style.css";
 
 export default function Homes() {
-  const [ProductoGuardados, setProductoGuardados] = useState([]);
-  const [value, setValue] = useState([30, 60]);
+	const [ProductoGuardados, setProductoGuardados] = useState([]);
+	const [value, setValue] = useState([30, 60]);
 
-  /*
+	/*
 	useEffect(() => {
 		const prices = ProductoGuardados.map(ProductoGuardados => parseFloat(ProductoGuardados.price));
 		const minPrice = Math.min(...prices);
@@ -19,32 +19,32 @@ export default function Homes() {
 		setValue([minPrice, maxPrice]);
 	  }, [ProductoGuardados]);*/
 
-  useEffect(() => {
-    fetch("http://20.231.216.22:5000/products")
-      .then((res) => res.json())
-      .then((data) => setProductoGuardados(data));
-  }, []);
+	useEffect(() => {
+		fetch("http://20.231.216.22:5000/products")
+			.then((res) => res.json())
+			.then((data) => setProductoGuardados(data));
+	}, []);
 
-  return (
-    <Layout>
-      <Row>
-        <Col xs={12} md={2}>
-          <div className={styles["sidebar-grid"]}>
-            <h2>Filtros:</h2>
-            <div>Precio</div>
-            <RangeSlider value={value} onInput={setValue} />
-            <ul className={styles["sidebar-list"]}>
-              <div class="container"></div>
-            </ul>
-          </div>
-          <Button variant="primary">Filtrar</Button>
-        </Col>
-        <Col xs={12} md={10}>
-          <div className={styles["content-grid"]}>
-            <ItemGrid items={ProductoGuardados} />
-          </div>
-        </Col>
-      </Row>
-    </Layout>
-  );
+	return (
+		<Layout>
+			<Row>
+				<Col xs={12} md={2}>
+					<div className={styles["sidebar-grid"]}>
+						<h2>Filtro:</h2>
+						<div>Precio</div>
+						<RangeSlider value={value} onInput={setValue} />
+						<ul className={styles["sidebar-list"]}>
+							<div class="container"></div>
+						</ul>
+					</div>
+					<Button variant="primary">Filtrar</Button>
+				</Col>
+				<Col xs={12} md={10}>
+					<div className={styles["content-grid"]}>
+						<ItemGrid items={ProductoGuardados} />
+					</div>
+				</Col>
+			</Row>
+		</Layout>
+	);
 }
